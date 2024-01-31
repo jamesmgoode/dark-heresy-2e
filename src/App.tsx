345fb5fe318f3881287c875888cedb4043a3a90b
James Goode <james_goode@hotmail.co.uk>
2024-01-31 13:18:24 +0000

    Add some nav items

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="nav-bar">
+          <a>Exp Costs</a>
+          <div>
+            <button>Combat</button>
+          </div>
+          <div>
+            <button>Critical Hits</button>
+          </div>
+          <div>
+            <button>Equipment</button>
+          </div>
+          <div>
+            <button>Weapons</button>
+          </div>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.

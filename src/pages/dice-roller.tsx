e8d4a3ee03499324eaa3148af31a7e681cab5ac8
James Goode <james_goode@hotmail.co.uk>
2024-01-31 18:41:48 +0000

    Add some json, make dice roller a component

--- src/pages/dice-roller.tsx
+++ src/pages/dice-roller.tsx
@@ -1,7 +1,34 @@
-export default function DiceRoller() {
-    return (
-        <div>
-            <h1>Dice Roller</h1>
-        </div>
-    );
+import Skills from '../data/skills.json';
+import Circumstances from '../data/circumstances.json';
+import React, { Component } from 'react';
+
+export default class DiceRoller extends Component {
+    constructor(props: any) {
+        super(props);
+        this.state = { selectedSkill: Skills[0].name };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Dice Roller</h1>
+                <div>
+                    <label htmlFor="skill-options">Skill: </label>
+                    <select id="skill-options">{
+                        Skills.map(skill =>
+                            <option key={skill.name}>{skill.name}</option>)
+                    }
+                    </select>
+                </div>
+                <table>{
+                    Circumstances.map(circumstance =>
+                        <tr>
+                            <td>{circumstance.name}</td>
+                            <td>{circumstance.effect}</td>
+                        </tr>)
+                }
+                </table>
+            </div>
+        );
+    }
 }
